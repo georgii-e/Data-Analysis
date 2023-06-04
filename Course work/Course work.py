e3ab979147083a3b95ee04c5cff70f5f611823f5
Yehor <90179279+georgii-e@users.noreply.github.com>
2023-06-04 12:47:05 +0300

    Add files via upload

--- Course work/Course work.py
+++ Course work/Course work.py
@@ -31,15 +31,15 @@ def correlations():
     plt.show()
 
 
-def choose_hyperparameters(log_reg):
+def choose_hyperparameters_log(log_reg):
     param_grid_1 = {
-        'C': [0.5, 1.0, 5, 10.0],
+        'C': [0.5, 1, 5, 10],
         'solver': ['liblinear', 'newton-cholesky'],
         'max_iter': [100, 300, 500]
     }
 
     param_grid_2 = {
-        'C': [0.5, 1.0, 5, 10.0],
+        'C': [0.5, 1, 5, 10],
         'solver': ['lbfgs'],
         'max_iter': [2500, 3000, 5000]
     }
@@ -49,7 +49,7 @@ def choose_hyperparameters(log_reg):
     print("Best hyperparameters from first grid:", grid_search.best_params_)
     best_model_1 = grid_search.best_estimator_
     accuracy_1 = best_model_1.score(X_test, y_test)
-    print(f'Test accuracy with best model: {accuracy_1 * 100:.2f}%')
+    print(f'Test accuracy with first model: {accuracy_1 * 100:.2f}%')
 
     grid_search = GridSearchCV(estimator=log_reg, param_grid=param_grid_2)
     grid_search.fit(X_train, y_train)
@@ -57,11 +57,27 @@ def choose_hyperparameters(log_reg):
     print("Best hyperparameters from second grid:", grid_search.best_params_)
     best_model_2 = grid_search.best_estimator_
     accuracy_2 = best_model_2.score(X_test, y_test)
-    print(f'Test accuracy with best model: {accuracy_2 * 100:.2f}%')
+    print(f'Test accuracy with second model: {accuracy_2 * 100:.2f}%')
 
     if accuracy_1 > accuracy_2:
         return best_model_1
     return best_model_2
+
+
+def choose_hyperparameters_dec(dec_tree):
+    param_grid = {
+        'criterion': ['gini', 'entropy', 'log_loss'],
+        'max_depth': [None, 3, 5, 7, 10],
+        'max_leaf_nodes': [None, 30, 50, 100]
+    }
+    grid_search = GridSearchCV(estimator=dec_tree, param_grid=param_grid)
+    grid_search.fit(X_train, y_train)
+    print("Best hyperparameters from the grid:", grid_search.best_params_)
+    best_model = grid_search.best_estimator_
+    accuracy = best_model.score(X_test, y_test)
+    print(f'Test accuracy with best model: {accuracy * 100:.2f}%')
+
+    return best_model
 
 
 def confusion_matrix_plot(classifier, test_x, test_y):
@@ -73,7 +89,7 @@ def confusion_matrix_plot(classifier, test_x, test_y):
     plt.show()
 
 
-initial_df = pd.read_csv(r"F:\Egor\Уроки\Аналіз даних\Курсова робота\Framingham.csv")
+initial_df = pd.read_csv(r"D:\Временные файлы\Framingham.csv")
 heart_disease = initial_df.copy()
 heart_disease.info()
 print(heart_disease.describe())
@@ -85,13 +101,23 @@ X = heart_disease.drop(columns='TenYearCHD')
 y = heart_disease.loc[:, 'TenYearCHD']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=11)
 
-default_log_reg = LogisticRegression(solver='liblinear')  # was default in previous version of linear regression;
+default_log_reg = LogisticRegression(solver='liblinear')  # was default solver in previous version of linear regression;
 # no convergence with lbfgs solver and default max_iter parameter
 default_log_reg.fit(X_train, y_train)
 log_accuracy = default_log_reg.score(X_test, y_test)
 print(f'Default logistic regression model accuracy: {log_accuracy * 100:.2f}%')
 
 logistic_regression = LogisticRegression()
-optimal_log_reg = choose_hyperparameters(logistic_regression)
+optimal_log_reg = choose_hyperparameters_log(logistic_regression)
 
 confusion_matrix_plot(optimal_log_reg, X_test, y_test)
+
+default_dec_tree = DecisionTreeClassifier()
+default_dec_tree.fit(X_train, y_train)
+dtree_accuracy = default_dec_tree.score(X_test, y_test)
+print(f'Default decision tree model accuracy: {dtree_accuracy * 100:.2f}%')
+
+decision_tree = DecisionTreeClassifier()
+optimal_dec_tree = choose_hyperparameters_dec(decision_tree)
+
+confusion_matrix_plot(optimal_dec_tree, X_test, y_test)
