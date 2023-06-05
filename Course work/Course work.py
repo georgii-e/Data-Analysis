31047dfdacfd7e9d3aaae176aeab3b7be2c42ce8
Yehor <90179279+georgii-e@users.noreply.github.com>
2023-06-05 19:41:36 +0300

    Add files via upload

--- Course work/Course work.py
+++ Course work/Course work.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from sklearn.metrics import f1_score
+from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import GridSearchCV
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
@@ -66,16 +69,47 @@ def choose_hyperparameters_log(log_reg):
 
 def choose_hyperparameters_dec(dec_tree):
     param_grid = {
-        'criterion': ['gini', 'entropy', 'log_loss'],
-        'max_depth': [None, 3, 5, 7, 10],
-        'max_leaf_nodes': [None, 30, 50, 100]
+        'n_estimators': [100, 200, 300],
+        'max_depth': [None, 5, 10],
+        'min_samples_split': [2, 5, 10],
     }
     grid_search = GridSearchCV(estimator=dec_tree, param_grid=param_grid)
     grid_search.fit(X_train, y_train)
     print("Best hyperparameters from the grid:", grid_search.best_params_)
     best_model = grid_search.best_estimator_
     accuracy = best_model.score(X_test, y_test)
-    print(f'Test accuracy with best model: {accuracy * 100:.2f}%')
+    print(f'Test accuracy with best Decision Tree model: {accuracy * 100:.2f}%')
+
+    return best_model
+
+
+def choose_hyperparameters_rand(rand_forest):
+    param_grid = {
+        'criterion': ['gini', 'entropy', 'log_loss'],
+        'max_depth': [None, 5, 10]
+    }
+    grid_search = GridSearchCV(estimator=rand_forest, param_grid=param_grid)
+    grid_search.fit(X_train, y_train)
+    print("Best hyperparameters from the grid:", grid_search.best_params_)
+    best_model = grid_search.best_estimator_
+    accuracy = best_model.score(X_test, y_test)
+    print(f'Test accuracy with best Random Forest model: {accuracy * 100:.2f}%')
+
+    return best_model
+
+
+def choose_hyperparameters_svc(svc):
+    param_grid = {
+        'C': [1, 3, 5, 10],
+        'degree': [3, 4, 5],
+        'gamma': ['scale', 'auto'],
+    }
+    grid_search = GridSearchCV(estimator=svc, param_grid=param_grid)
+    grid_search.fit(X_train, y_train)
+    print("Best hyperparameters from the grid:", grid_search.best_params_)
+    best_model = grid_search.best_estimator_
+    accuracy = best_model.score(X_test, y_test)
+    print(f'Test accuracy with best SVC model: {accuracy * 100:.2f}%')
 
     return best_model
 
@@ -89,7 +123,7 @@ def confusion_matrix_plot(classifier, test_x, test_y):
     plt.show()
 
 
-initial_df = pd.read_csv(r"D:\Временные файлы\Framingham.csv")
+initial_df = pd.read_csv(r"F:\Egor\Уроки\Аналіз даних\Курсова робота\Framingham.csv")
 heart_disease = initial_df.copy()
 heart_disease.info()
 print(heart_disease.describe())
@@ -106,18 +140,50 @@ default_log_reg = LogisticRegression(solver='liblinear')  # was default solver i
 default_log_reg.fit(X_train, y_train)
 log_accuracy = default_log_reg.score(X_test, y_test)
 print(f'Default logistic regression model accuracy: {log_accuracy * 100:.2f}%')
+f1 = f1_score(y_test, default_log_reg.predict(X_test))
+print("F1 Score for default decision tree model:", f1)
 
 logistic_regression = LogisticRegression()
 optimal_log_reg = choose_hyperparameters_log(logistic_regression)
-
+f1 = f1_score(y_test, optimal_log_reg.predict(X_test))
+print("F1 Score for optimal decision tree model:", f1)
 confusion_matrix_plot(optimal_log_reg, X_test, y_test)
 
 default_dec_tree = DecisionTreeClassifier()
 default_dec_tree.fit(X_train, y_train)
 dtree_accuracy = default_dec_tree.score(X_test, y_test)
 print(f'Default decision tree model accuracy: {dtree_accuracy * 100:.2f}%')
+f1 = f1_score(y_test, default_dec_tree.predict(X_test))
+print("F1 Score for default decision tree model:", f1)
 
 decision_tree = DecisionTreeClassifier()
 optimal_dec_tree = choose_hyperparameters_dec(decision_tree)
-
+f1 = f1_score(y_test, optimal_dec_tree.predict(X_test))
+print("F1 Score for optimal decision tree model:", f1)
 confusion_matrix_plot(optimal_dec_tree, X_test, y_test)
+
+default_rand_forest = RandomForestClassifier()
+default_rand_forest.fit(X_train, y_train)
+rforest_accuracy = default_rand_forest.score(X_test, y_test)
+print(f'Default random forest model accuracy: {rforest_accuracy * 100:.2f}%')
+f1 = f1_score(y_test, default_rand_forest.predict(X_test))
+print("F1 Score for default random forest model:", f1)
+
+random_forest = RandomForestClassifier()
+optimal_rand_forest = choose_hyperparameters_rand(random_forest)
+f1 = f1_score(y_test, optimal_rand_forest.predict(X_test))
+print("F1 Score for optimal random forest model:", f1)
+confusion_matrix_plot(optimal_rand_forest, X_test, y_test)
+
+default_svc = SVC()
+default_svc.fit(X_train, y_train)
+svc_accuracy = default_svc.score(X_test, y_test)
+print(f'Default SVC model accuracy: {svc_accuracy * 100:.2f}%')
+f1 = f1_score(y_test, default_svc.predict(X_test))
+print("F1 Score for default SVC model:", f1)
+
+SVC = SVC()
+optimal_svc = choose_hyperparameters_svc(SVC)
+f1 = f1_score(y_test, optimal_svc.predict(X_test))
+print("F1 Score for optimal SVC model:", f1)
+confusion_matrix_plot(optimal_svc, X_test, y_test)
